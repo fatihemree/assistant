32aff74591de69a54866c8d5f95e7c01e17804ba
fatihemree <fatih.emre.212@gmail.com>
2020-05-12 01:39:20 +0300

    logo | info | menu konumları ayarlandı

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'WoTAssistant',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('WoT Assistant'),
-        ),
         body: Center(
           child: Container(
-            child: Row(
-             // verticalDirection: VerticalDirection.down,
-              children: <Widget>[
-                logo,
-              ],
+            decoration: conStyle,
+            child: Column(
+              children: <Widget>[topBarButton, content],
             ),
           ),
         ),
@@ -25,8 +20,46 @@ class MyApp extends StatelessWidget {
     );
   }
 
+  Widget get content => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          logo,
+        ],
+      );
   Widget get logo => Image.asset(
         'assets/img/logo.png',
-        width: 300,
+        width: 250,
+        fit: BoxFit.cover,
       );
+
+  BoxDecoration conStyle = BoxDecoration(
+    image: DecorationImage(
+      image: AssetImage("assets/img/bg.png"),
+      fit: BoxFit.cover,
+    ),
+  );
+  Widget get infoButton => FlatButton(
+      onPressed: null,
+      child: Icon(
+        Icons.info_outline,
+        size: 35,
+        color: Colors.white70,
+      ));
+  Widget get sideBarButton => FlatButton(
+      onPressed: null,
+      child: Icon(Icons.menu, size: 35, color: Colors.white70));
+
+  Widget get topBarButton => Padding(
+        padding: const EdgeInsets.only(top: 25, bottom: 50),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[sideBarButton, infoButton],
+        ),
+      );
+
+  Widget get loginInputUser => TextField(
+      onChanged: (val) {},
+      textAlign: TextAlign.left,
+      style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600));
 }
